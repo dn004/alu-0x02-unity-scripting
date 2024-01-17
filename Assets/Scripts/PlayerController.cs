401d7e3d7c8a5a7b094571632975e29dd8dd3d6a
dn004 <d.ntwari@alustudent.com>
2024-01-17 14:18:50 +0200

    1 done

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 1000f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Get input for movement
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Calculate movement vector (no vertical movement)
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        // Apply force to the rigidbody for movement with Time.deltaTime
+        rb.AddForce(movement * speed * Time.deltaTime);
+
+        
+    }
+}
